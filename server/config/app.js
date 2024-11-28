768253193bc919b69e84446a6c3e79e8514e9b82
dalemalapitan <dale.malapitan@ontariotechu.net>
2024-11-27 21:44:11 -0500

    auth pt1

--- server/config/app.js
+++ server/config/app.js
@@ -4,7 +4,41 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// module dependencies for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
 let app = express();
+
+
+// set up express session
+app.use(session({
+  secret:"SomeSecret",
+  saveUninitialized:false,
+  resave:false
+}));
+
+// initialize flash
+app.use(flash());
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// create a User model
+let userModel = require('../model/user');
+let User = userModel.User;
+
+// serialize and deserialize the User info
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
 let bookRouter = require('../routes/book');
