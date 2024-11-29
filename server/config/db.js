286ff99f4c3c59a2e21955671f820969c115dfea
DarynK14 <daryn.king@ontariotechu.net>
2024-11-28 19:15:34 -0500

    update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-module.exports={"URI":"mongodb+srv://Server:XYZserver123@cluster0.qltkc.mongodb.net/TaskMaster"}
+module.exports={"URI":"mongodb+srv://Server:XYZserver123@cluster0.qltkc.mongodb.net/TaskMaster?retryWrites=true&w=majority"}
 
 
 
